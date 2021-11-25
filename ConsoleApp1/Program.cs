5e1897e993d4e9ee0386b404f9539be27213bf3f
Виктор Парыгин <paryginva@live.com>
2021-11-25 21:49:01 +0500

    Закончено

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double V, S;
+            CalcCubeVolume_n_Area(a, out V, out S);
+
+        }
+    }
+}
---
+++ Task7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину ребра куба: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double V, S;
+            CalcCubeVolume_n_Area(a, out V, out S);
+            Console.WriteLine("Площадь поверхности куба: {0}\nОбъем куба: {1}",S,V);
+            Console.ReadKey();
+        }
+        static void CalcCubeVolume_n_Area(double a, out double V, out double S)
+        {
+            V = a*a*a;
+            S = 6*a*a;
+        }
+    }
+}
